7d22d7aae0b547d462fe20063a4f0e8ec76b2f85
duttabhishek32 <duttabhishek0@gmail.com>
2021-05-29 23:38:12 +0530

    Initial Commit

---
+++ app/src/main/java/com/abhishek/myapplication/CenterRvModel.kt
@@ -0,0 +1,12 @@
+package com.abhishek.myapplication
+
+data class CenterRvModel(
+    val centerName: String,
+    val centerAddress: String,
+    val centerFromTiming: String,
+    var centerToTiming: String,
+    var feeType: String,
+    var ageLimit: Int,
+    var vaccineName: String,
+    var availableCapacity: Int
+)
