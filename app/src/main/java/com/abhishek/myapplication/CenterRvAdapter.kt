b52e3d2f36c22000aa92e38b97b256f31396caf8
vibyuti <79533309+vibyuti@users.noreply.github.com>
2021-08-27 08:04:11 +0530

    Update CenterRvAdapter.kt

--- app/src/main/java/com/abhishek/myapplication/CenterRvAdapter.kt
+++ app/src/main/java/com/abhishek/myapplication/CenterRvAdapter.kt
@@ -5,7 +5,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-//import com.mohitsharda.whereismyvaccine.R
 
 import com.abhishek.myapplication.R
 
